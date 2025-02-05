10f2d94727f125a199758539968057bfa8ec564a
Mikhail Belozerov <belozerov.mg@gmail.com>
2025-02-05 21:40:43 +0700

    modified routes.php and Router.php

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,6 +4,19 @@ namespace App\Router;
 
 class Router
 {
+
+    // массив для хранения определенных групп маршрутов
+    private array $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
+
+
+    public function __construct()
+    {
+        $this->initRoutes();
+    }
+
     public function dispatch(string $url): void
     {
         // получение всех маршрутов
@@ -13,6 +26,23 @@ class Router
         $routes[$url]();
     }
 
+    // расфасовывает маршруты из файла routes.php по группам
+    private function initRoutes() {
+
+        $routes = $this->getRoutes();
+
+        foreach ($routes as $route) {
+            $this->routes[$route->getMethod()][$route->getUrl()] = $route;
+        }
+
+    }
+
+    // получает массив всех маршрутов
+    // @return -> явно показывает что возвращаем массив из роутов
+
+    /**
+     * @return Route[]
+     */
     private function getRoutes(): array
     {
         return require_once APP_PATH . '/routes/routes.php';
