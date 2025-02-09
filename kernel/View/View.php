0cb4ef7c4cc3ce6a5ee37a032ea49a05b27e8975
Mikhail Belozerov <belozerov.mg@gmail.com>
2025-02-09 15:26:33 +0700

    template refactoring and parameter injection

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -8,6 +8,16 @@ class View
     // отображает страницу переданную в параметрах
     public function page(string $name): void
     {
-        include_once APP_PATH."/views/pages/$name.php";
+        // внедрение переменных в шаблоны. То что указано как ключ - является названием переменной для использ. в шаблоне
+        extract([
+            'view' => $this
+        ]);
+
+        include_once APP_PATH . "/views/pages/$name.php";
+    }
+
+    public function component(string $name): void
+    {
+        include_once APP_PATH . "/views/components/$name.php";
     }
 }
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,10 @@
-<?php include __DIR__ . '/../components/start.php' ?>
+<?php
+// подсказка для php с помощью php docs (чтобы не было ошибок и php корректно определял какого типа переменная $view)
+/**
+ * @var \App\Kernel\View\View $view
+ */
+?>
+
+<?php $view->component('start') ?>
 <h1>Home page</h1>
-<?php include __DIR__ . '/../components/end.php' ?>
+<?php $view->component('end') ?>
