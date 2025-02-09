28702f85a5c8eb33dea64790cbdb08d0e09e082b
Mikhail Belozerov <belozerov.mg@gmail.com>
2025-02-09 15:42:09 +0700

    Check for routing to a non-existent page

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -8,6 +8,13 @@ class View
     // отображает страницу переданную в параметрах
     public function page(string $name): void
     {
+
+        $viewPath = APP_PATH . "/views/pages/$name.php";
+
+        if (!file_exists($viewPath)) {
+            throw new \Exception('View $name not found');
+        }
+
         // внедрение переменных в шаблоны. То что указано как ключ - является названием переменной для использ. в шаблоне
         extract([
             'view' => $this
