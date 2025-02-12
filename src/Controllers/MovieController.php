129009528669240356f823d0728c723f312fe99a
Mikhail Belozerov <belozerov.mg@gmail.com>
2025-02-12 21:08:14 +0700

    refactor Validator class and testing validation

--- src/Controllers/MovieController.php
+++ src/Controllers/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Kernel\Controller\Controller;
+use App\Kernel\Validator\Validator;
 use App\Kernel\View\View;
 
 class MovieController extends Controller
@@ -19,6 +20,13 @@ class MovieController extends Controller
 
     public function store()
     {
+        $data = ['name' => 'Mikhail'];
+        $rules = ['name' => ['required', 'min:3', 'max:255']];
+
+        $validator = new Validator();
+
+        dd($validator->validate($data, $rules), $validator->errors());
+
         dd($this->getRequest()->input('name'));
     }
 }
